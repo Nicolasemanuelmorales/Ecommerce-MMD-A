9989b14a125406ce28bfda82dbb612946b211a83
Nicolas Emanuel Morales <nicolasemanuelmorales@hotmail.com>
2019-07-17 12:26:18 -0300

    no se

---
+++ src/test/java/ar/edu/unlam/tallerweb1/controlador/TestControladorShop.java
@@ -0,0 +1,41 @@
+package ar.edu.unlam.tallerweb1.controlador;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unlam.tallerweb1.controladores.ControladorLogin;
+import ar.edu.unlam.tallerweb1.controladores.ControllerProductos;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import ar.edu.unlam.tallerweb1.servicios.ServicioLogin;
+import ar.edu.unlam.tallerweb1.servicios.ServicioProducto;
+
+public class TestControladorShop {
+
+	@Test
+	public void TestFiltraProducto() {
+		
+		
+		String filtro="bermuda";
+		Double formaDeFiltro = 40.0;
+		Double formaDeFiltro2 = 50.0;
+		String ordenado = "mayor";
+		
+		ControllerProductos sut = new ControllerProductos();
+			
+		ServicioProducto servicioProducto = mock(ServicioProducto.class);
+		
+		sut.setservicioProducto(servicioProducto);
+		when(servicioProducto.filtrarProductoPor(filtro,formaDeFiltro,formaDeFiltro2,ordenado)).thenReturn(null);
+		
+		ModelAndView modelo = sut.filtrarPor(filtro,formaDeFiltro,formaDeFiltro2,ordenado);
+		
+		assertThat(modelo.getViewName()).isEqualTo("shop");
+		
+	}
+}
